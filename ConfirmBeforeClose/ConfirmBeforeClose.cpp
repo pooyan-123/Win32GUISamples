de61f9230c1a58e30cea3cf7383549e96cf61752
pooyan-123 <82775880+pooyan-123@users.noreply.github.com>
2023-07-06 06:18:32 +0430

    Added ConfirmBeforeClose project

---
+++ ConfirmBeforeClose/ConfirmBeforeClose.cpp
@@ -0,0 +1,62 @@
+#include <windows.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	HWND hWnd;
+	WNDCLASS wndclass;
+	BOOL bRet;
+	MSG msg;
+
+	wndclass.lpszClassName = TEXT("MyClass");
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.style = 0;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hInstance = hInstance;
+	if (!RegisterClass(&wndclass))
+		return 0;
+
+	hWnd = CreateWindow(TEXT("MyClass"), TEXT("SimpleWindow"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+	if (!hWnd)
+		return 0;
+
+	ShowWindow(hWnd, nCmdShow);
+	if (!UpdateWindow(hWnd))
+		return 0;
+
+	while ((bRet = GetMessage(&msg, NULL, 0, 0)) != 0)
+	{
+		if (bRet == -1)
+			return 0;
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int msgboxID;
+
+	switch (message)
+	{
+	case WM_CLOSE:
+		msgboxID = MessageBox(hWnd, TEXT("Do you really want to close the window?"), TEXT("Close"), MB_YESNO | MB_ICONEXCLAMATION);
+		if (msgboxID == IDYES)
+			DestroyWindow(hWnd);
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
