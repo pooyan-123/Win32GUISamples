c3f47d42bfc6ddc2a4a3ce3162f268d74c8bacd4
pooyan-123 <82775880+pooyan-123@users.noreply.github.com>
2023-08-05 21:00:52 +0430

    Added project TextOutAtoZVariant1

---
+++ TextOutAtoZVariant1/TextOutAtoZVariant1.cpp
@@ -0,0 +1,79 @@
+#include <windows.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+const TCHAR szAtoZ[] = TEXT("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASS wc = { 0 };
+	HWND hWnd;
+	BOOL bRet;
+	MSG msg;
+
+	wc.lpszClassName = TEXT("MyClass");
+	wc.lpfnWndProc = WndProc;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.hInstance = hInstance;
+	if (!RegisterClass(&wc))
+		return 0;
+
+	hWnd = CreateWindow(TEXT("MYCLASS"), TEXT("TextOutAtoZVariant1"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+	if (!hWnd)
+		return 0;
+
+	ShowWindow(hWnd, nCmdShow);
+	if (!UpdateWindow(hWnd))
+		return 0;
+
+	while ((bRet = GetMessage(&msg, NULL, 0, 0)) != 0)
+	{
+		if (bRet == -1)
+			return 0;
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HDC hDC;
+	TEXTMETRIC tm;
+	static LONG cyChar;
+	PAINTSTRUCT ps;
+	const size_t iLastLine = (sizeof(szAtoZ) / sizeof(TCHAR)) - 2;
+	size_t iFirstLineToPaint;
+	size_t iLastLineToPaint;
+	switch (message)
+	{
+	case WM_CREATE:
+		hDC = GetDC(hWnd);
+		if (!GetTextMetrics(hDC, &tm))
+			return -1;
+		cyChar = tm.tmHeight + tm.tmExternalLeading;
+		ReleaseDC(hWnd, hDC);
+		return 0;
+
+	case WM_PAINT:
+		hDC = BeginPaint(hWnd, &ps);
+		iFirstLineToPaint = ps.rcPaint.top / cyChar;
+		iLastLineToPaint = min((ps.rcPaint.bottom / cyChar) + 1, iLastLine);
+		if (iFirstLineToPaint <= iLastLine)
+			for (size_t i = iFirstLineToPaint; i <= iLastLineToPaint; i++)
+			{
+				TextOut(hDC, 30, i * cyChar, &szAtoZ[i], 1);
+			}
+		EndPaint(hWnd, &ps);
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
